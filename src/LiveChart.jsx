e443dcc9e4f02312399ccb632a922404d216ec7c
lucadaniel2345-ctrl <lucadaniel2345@gmail.com>
2026-03-19 16:13:36 -0700

    Update LiveChart.jsx

--- src/LiveChart.jsx
+++ src/LiveChart.jsx
@@ -8,7 +8,7 @@ useEffect(()=>{
 
 const fetchPrice = () => {
 
-fetch("http://bestcryptotrading.rf.gd/backend/api/get_prices.php",{
+fetch("https://bestcryptotrading.rf.gd/backend/api/get_prices.php",{
 credentials:"include"
 })
 .then(res=>res.json())
