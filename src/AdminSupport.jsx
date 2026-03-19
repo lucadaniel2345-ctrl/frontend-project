8d9cef135fdab093336e24fdb1b109d35971761b
lucadaniel2345-ctrl <lucadaniel2345@gmail.com>
2026-03-19 16:06:18 -0700

    Update AdminSupport.jsx

--- src/AdminSupport.jsx
+++ src/AdminSupport.jsx
@@ -4,7 +4,7 @@ export default function AdminSupport() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("http://bestcryptotrading.rf.gd/backend/api/adminUsers.php")
+    fetch("https://bestcryptotrading.rf.gd/backend/api/adminUsers.php")
       .then(r => r.json())
       .then(setUsers);
   }, []);
