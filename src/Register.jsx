51dfae8788aca8b69253bcd1975877a73152a766
lucadaniel2345-ctrl <lucadaniel2345@gmail.com>
2026-03-19 16:16:44 -0700

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -18,7 +18,7 @@ export default function Register({ goLogin }) {
     setMessage("Processing registration...");
 
     try {
-      const res = await fetch("http://bestcryptotrading.rf.gd/backend/api/register.php", {
+      const res = await fetch("https://bestcryptotrading.rf.gd/backend/api/register.php", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
